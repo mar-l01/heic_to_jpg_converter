633f72785657186db7e5c434acf2803f8c0e0c57
Markus Loipfinger <m.loipfinger@hotmail.de>
2025-03-03 17:11:09 +0100

    add script to convert images

---
+++ heic_to_jpg_converter.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from pathlib import Path
+from tqdm import tqdm
+
+# required to open heic-formatted images
+from pillow_heif import register_heif_opener
+register_heif_opener()
+
+HEIC_DIRECTORY = Path("<input directory>")
+JPG_DIRECTORY = Path("<output directory>")
+
+for image_file in tqdm(HEIC_DIRECTORY.glob("*.HEIC")):
+    heic_image = Image.open(image_file)
+    jpg_image_name = JPG_DIRECTORY / f"{image_file.stem}.jpg"
+    heic_image.save(jpg_image_name, "JPEG", quality=100)
+ 
+input("Press Enter to exit...")
